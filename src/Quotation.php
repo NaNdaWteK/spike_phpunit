2111d4b9fd4b85cfae865c64faf6254e97571a77
Nacho <nandawtek@gmail.com>
2017-09-07 07:09:23 +0200

    Refactor quotation

--- src/Quotation.php
+++ src/Quotation.php
@@ -4,26 +4,35 @@ namespace Spike;
 class Quotation
 {
     const API = 'http://api.fixer.io/latest';
-    protected $oficial;
-    protected $blue;
+    protected $dolar;
 
     public function __construct()
     {
         $data_in = self::API;
         $data_json = @file_get_contents($data_in);
-        if(self::hasContent($data_json))
-        {
-            $data_out = json_decode($data_json, true);
-            if(is_array($data_out))
-            {
-                $this->oficial = $data_out['rates']['USD'];
-            }
-        }
+        self::dolarize($data_json);
     }
 
     public function convertToOficialDolar($price)
     {
-        return $price * $this->oficial;
+        return $price * $this->dolar;
+    }
+
+    private function dolarize($json)
+    {
+        if(self::hasContent($json))
+        {
+            $data_out = json_decode($json, true);
+            self::setDolar($data_out);
+        }
+    }
+
+    private function setDolar($data)
+    {
+        if(is_array($data))
+        {
+            $this->dolar = $data['rates']['USD'];
+        }
     }
 
     private function hasContent($data)
